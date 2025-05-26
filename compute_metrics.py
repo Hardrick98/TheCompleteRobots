3b1254fc0083e5916c9dce21d4acc266baaaaaea
Riccardo <riccardocatalini@gmail.com>
2025-05-26 17:10:56 +0200

    Paper related Analysis

--- compute_metrics.py
+++ compute_metrics.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 
     
-data = {"Robot":[],"LLM":[],"Pose":[],"Error":[]}
+data = {"Robot":[],"LLM":[],"Pose":[],"Avg1":[], "Avg2":[],"Sum1":[], "Sum2":[]}
+data = {"Robot":[],"LLM":[],"Pose":[], "GT_count":[], "Pred_count":[]}
 
 for robot in ["nao", "atlas","h12","khr3hv"]:
     with open(f'json/GT_{robot}.json', 'r') as f:
@@ -21,30 +22,58 @@ for robot in ["nao", "atlas","h12","khr3hv"]:
 
             gt_pose_array = []
             pred_pose_array = []
+            pred_pose_array2 = []
                 
             for k, v in gt_pose.items():
                 gt_pose_array.append(v)
                 if k in pred_pose:
                     pred_value = pred_pose[k]
-                    pred_pose_array.append(pred_value if v != 0 else 0)
+                    pred_pose_array.append(pred_value) #Considers all the joints
                 else:
                     pred_pose_array.append(0)
+                    
                    
                 
             
             gt_pose = np.array(gt_pose_array)
             pred_pose = np.array(pred_pose_array)
-
-            total_error = np.sum(np.abs(gt_pose - pred_pose), axis=0)
+            mask = (gt_pose != 0)
+            mask2 = (pred_pose != 0) 
+            
+            a = np.sum(mask)
+            b = np.sum(mask2)
+            
+            data["Robot"].append(robot)
+            data["LLM"].append(llm)
+            data["Pose"].append(p)
+            data["GT_count"].append(a)
+            data["Pred_count"].append(b)
+            """
+            if np.sum(mask) == 0:
+                total_error_mean = 0
+                total_error_sum = 0
+            else:
+                total_error_sum = np.sum(np.abs(gt_pose[mask] - pred_pose[mask]))
+                total_error_mean = np.mean(np.abs(gt_pose[mask] - pred_pose[mask]))
+            
+            mask = (gt_pose != 0) | ((gt_pose == 0) & (pred_pose != 0)) #active or moved joints 
+            if np.sum(mask) == 0:
+                total_error_sum2 = 0
+                total_error_mean2 = 0
+            else:
+                total_error_mean2 = np.mean(np.abs(gt_pose[mask] - pred_pose[mask]))
+                total_error_sum2 = np.sum(np.abs(gt_pose[mask] - pred_pose[mask]))
             
             data["Robot"].append(robot)
             data["Pose"].append(p)
             data["LLM"].append(llm)
-            data["Error"].append(total_error)
-            
+            data["Avg1"].append(total_error_mean)
+            data["Avg2"].append(total_error_mean2)           
+            data["Sum1"].append(total_error_sum)
+            data["Sum2"].append(total_error_sum2) 
+            """
 
-
-pd.DataFrame(data).to_csv("analysis.csv", index=False)
+pd.DataFrame(data).to_csv("analysis_count.csv", index=False)
         
     
     
