9344eb464df8b7d7c5f03065d465d3ed3e59e847
hardrick <riccardocatalini@gmail.com>
2025-06-24 15:32:25 +0200

    Fixed some bugs

--- robot_loader.py
+++ robot_loader.py
@@ -86,10 +86,12 @@ for i, joint_name in enumerate(model.names):
         if i != 0:
             end_effectors.append(joint_name)
 
-print("End effectors trovati:")
-for ee in end_effectors:
-    print(f"- {ee}")
+print(end_effectors)
 
+repeated = []
+
+
+    
 #FIND KINEMATIC CHAINS
 
 def get_kinematic_chains(end_effectors, parent_child):
@@ -110,11 +112,73 @@ def get_kinematic_chains(end_effectors, parent_child):
 chains = get_kinematic_chains(end_effectors, parent_child)
 
 
+
+
 new_chains = []
 for chain in chains:
-    
     new_chains.append([model.getJointId(joint) for joint in chain if model.getJointId(joint)!=1])
 
+print("Before Rectifing End-effector")
+print(new_chains)
+
+### Questo serve per togliere end-effector minori, tipo le dita, ma se il root non è il vero root crea problemi. VEDASI ATLAS. Atlas non ha le
+### catene cinematiche delle braccia attaccate al root questo vanifica queste righe di codice dopo.
+
+def longest_common_prefix(lists):
+    if not lists:
+        return []
+    prefix = lists[0]
+    for lst in lists[1:]:
+        i = 0
+        while i < len(prefix) and i < len(lst) and prefix[i] == lst[i]:
+            i += 1
+        prefix = prefix[:i]
+    return prefix
+
+def reduce_lists(input_lists, max_groups=5):
+    reduced = []
+    used = [False] * len(input_lists)
+    groups = []
+
+    for i in range(len(input_lists)):
+        if used[i]:
+            continue
+        group = [input_lists[i]]
+        used[i] = True
+        for j in range(i + 1, len(input_lists)):
+            if not used[j]:
+                prefix = longest_common_prefix([input_lists[i], input_lists[j]])
+                if prefix and len(prefix) >= 3:
+                    group.append(input_lists[j])
+                    used[j] = True
+        reduced.append(longest_common_prefix(group))
+        groups.append(group)
+
+    return reduced[:max_groups], groups[:max_groups]
+
+new_chains, groups = reduce_lists(new_chains, max_groups=5)
+
+
+
+print("After Rectifing End-effector")
+print(new_chains)
+
+## HANDLING DUPLICATE ORIGIN JOINTS DIFFERENT FROM ROOT (PROBLEM ATLAS CASE)
+
+filter = []
+for chain in new_chains:
+    filter.append(chain[0])
+
+values_to_remove = set()
+for chain in new_chains:
+    if filter.count(chain[0]) > 1:
+        values_to_remove.add(chain[0])
+        
+for chain in new_chains:
+    for v in values_to_remove:
+        if v in chain:
+            chain.remove(v)
+ 
 
 positions = []
 for chain in new_chains:
@@ -135,6 +199,10 @@ for chain in new_chains:
         positions[model.names[j]] = data.oMi[j].translation
     chains_new.append(positions)
 
+
+if len(chains_new) == 4:
+    head = [{}]
+    chains_new = head + chains_new
 ##CLUSTERIZE TO UNDERSTAND WHICH DEGREE OF FREEDOM IS RESPONSIBLE FOR A SPECIFIC LIMB
 
 robotoid = {}
@@ -143,7 +211,6 @@ robotoid_labels = {}
 chain_value = 0
 for positions in chains_new:
     
-    print("\n\n")
     
     filtered_data = {k: v for k, v in positions.items()}
 
@@ -155,6 +222,12 @@ for positions in chains_new:
     if points.shape[0] > 3:
         kmeans = KMeans(n_clusters=3, random_state=0).fit(points)
     else:
+        if len(points) == 0:
+            print("Head not movable")
+            robotoid[chain_value] = np.array([[100,100,100]])
+            robotoid_labels[chain_value] = [[None]]
+            chain_value += 1
+            continue
         kmeans = KMeans(n_clusters=1,random_state=0).fit(points)
 
     centroids = kmeans.cluster_centers_
@@ -177,21 +250,34 @@ for positions in chains_new:
             
     chain_value += 1
     
-
+order = []
+for j in new_chains:
+    for i in j:
+        order.append(i)
+    
+print("Order", order)
 
 for chain_id in robotoid:
-    # Prendi i centroidi e le label
     centers = robotoid[chain_id]
     labels = robotoid_labels[chain_id]
     
-    # Ordina per la coordinata z ([:, 2])
-    sorted_indices = np.argsort(centers[:, 2])[::-1] 
-    
-    # Applica l'ordinamento
+    if labels[0][0] is None:
+        continue
+
+    sorted_indices = sorted(range(len(labels)), key=lambda i: order.index(model.getJointId(labels[i][0])))
+
     robotoid[chain_id] = centers[sorted_indices]
     robotoid_labels[chain_id] = [labels[i] for i in sorted_indices]
 
+
+
+
+
+
+
 sorted_chain_ids = sorted(robotoid.keys(), key=lambda i: robotoid[i][0, 2], reverse=True)
+
+print(sorted_chain_ids)
 robotoid = {i: robotoid[i] for i in sorted_chain_ids}
 robotoid_labels = {i: robotoid_labels[i] for i in sorted_chain_ids}
 
@@ -200,12 +286,14 @@ new_robotoid_labels = {i: v for i, (_, v) in enumerate(robotoid_labels.items())}
    
 robotoid = new_robotoid
 robotoid_labels = new_robotoid_labels 
-print(robotoid)
-print(robotoid_labels)
+
+print("\n")
+print("Defined Chains:\n", robotoid_labels)
+print("\n")
 
 final = {}
 
-final["Head"] = robotoid_labels[0]
+final["Head"] = robotoid_labels[0][0]
 
 if robotoid[1][0,1] > 0:
     final["LShoulder"] = robotoid_labels[1][0]
@@ -213,14 +301,14 @@ if robotoid[1][0,1] > 0:
     final["LWrist"] = robotoid_labels[1][2]
     final["RShoulder"] = robotoid_labels[2][0]
     final["RElbow"] = robotoid_labels[2][1]
-    final["LWrist"] = robotoid_labels[2][2]
+    final["RWrist"] = robotoid_labels[2][2]
 else:
     final["LShoulder"] = robotoid_labels[2][0]
     final["LElbow"] = robotoid_labels[2][1]
     final["LWrist"] = robotoid_labels[2][2]
     final["RShoulder"] = robotoid_labels[1][0]
     final["RElbow"] = robotoid_labels[1][1]
-    final["LWrist"] = robotoid_labels[1][2]
+    final["RWrist"] = robotoid_labels[1][2]
     
     
 if robotoid[3][0,1] > 0:
@@ -237,5 +325,16 @@ else:
     final["RHip"] = robotoid_labels[3][0]
     final["RKnee"] = robotoid_labels[3][1]
     final["RAnkle"] = robotoid_labels[3][2]
-    
-print(final)
+
+
+print("Robotoid Associations:\n", final)
+
+
+joint_positions = {}
+for k, o in final.items():
+    for j in o:
+        if j is not None:
+            i = model.getJointId(j)
+            pos = data.oMi[i].translation
+            joint_positions[j] = pos
+
