1608c94797232531184f762f71e701cc9bdd3a67
Riccardo <riccardocatalini@gmail.com>
2025-10-15 15:03:35 +0200

    Rectified Nao mask and bbs

--- compute_data.sh
+++ compute_data.sh
@@ -1,28 +1,28 @@
-#echo "Starting Retargeting"
-#
-#python retarget_motion.py --robot $3 --interaction $1
-#python robot_interaction.py --robot1 $3 --robot2 $3 --interaction $1
-#
-#echo "Rendering videos..."
-#
-#python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode exoR
-#python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego1R
-#python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego2R
-#python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode exoL
-#python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego1L
-#python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego2L
-#
-#echo "Computing Bounding Boxes..."
+echo "Starting Retargeting"
+
+python retarget_motion.py --robot $3 --interaction $1
+python robot_interaction.py --robot1 $3 --robot2 $3 --interaction $1
+
+echo "Rendering videos..."
+
+python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode exoR
+python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego1R
+python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego2R
+python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode exoL
+python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego1L
+python render.py --interaction $1 --robot1 $3 --robot2 $3 --scene $2 --video $1 --camera_mode ego2L
+
+echo "Computing Bounding Boxes..."
 
 
 python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode exoR --bb_mode1
 python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode exoR --bb_mode2
 python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode exoL --bb_mode1
 python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode exoL --bb_mode2
-python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego1L --bb_mode1
-python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego2L --bb_mode2
-python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego1R --bb_mode1
-python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego2R --bb_mode2
+python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego1L --bb_mode2
+python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego2L --bb_mode1
+python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego1R --bb_mode2
+python extract_bb.py --interaction $1 --robot1 $3 --robot2 $3 --green_screen --camera_mode ego2R --bb_mode1
 
 
 
